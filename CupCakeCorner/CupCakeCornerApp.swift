1e543d19bbd498093bc50b694ecdabcb3e11840f
Daniel JCH <danielj.callisayah@gmail.com>
2022-02-22 12:52:56 -0400

    Initial Commit

---
+++ CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 22/2/22.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
