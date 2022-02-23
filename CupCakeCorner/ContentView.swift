41cccfc5844396cc76b7633009c61a862c95b251
Daniel JCH <danielj.callisayah@gmail.com>
2022-02-22 23:54:33 -0400

    Example

--- CupCakeCorner/ContentView.swift
+++ CupCakeCorner/ContentView.swift
@@ -8,26 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var results = [Result]()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
     }
 }
-enum CodingKeys: CodingKey {
-    case name
+
+
+struct Response: Codable {
+    var results: [Result]
 }
-class User: ObservableObject, Codable {
-    @Published var name = "Paul Hudson"
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
