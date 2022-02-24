c9bf2509c2d6fdd2a3185bf6b2bb1ab087c875aa
Daniel JCH <danielj.callisayah@gmail.com>
2022-02-23 20:34:00 -0400

    First Views

--- CupCakeCorner/ContentView.swift
+++ CupCakeCorner/ContentView.swift
@@ -8,24 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
-enum CodingKeys: CodingKey {
-    case name
-}
-class User: ObservableObject, Codable {
-    @Published var name = "Paul Hudson"
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
+    @StateObject var order = Order()
     
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
+    var body: some View {
+        NavigationView{
+            Form {
+                Section {
+                    Picker("Select your cake type ", selection: $order.type) {
+                        ForEach(Order.types.indices) {
+                                          Text(Order.types[$0])
+                                      }
+                    }
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                
+                Section {
+                    Toggle("Any special request?", isOn: $order.specialRequestEnabled.animation())
+                    if order.specialRequestEnabled {
+                          Toggle("Add extra frosting", isOn: $order.extraFrosting)
+
+                          Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                      }
+                }
+                Section {
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery details")
+                    }
+                }
+                
+            }
+            .navigationTitle("CupCake Corner")
+        }
     }
 }
 
